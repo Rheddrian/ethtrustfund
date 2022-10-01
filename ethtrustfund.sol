e90fe86888352fa6f847c11af639b8f5e2cf92a5
Rheddrian <98230845+Rheddrian@users.noreply.github.com>
2022-10-01 11:43:55 -0400

    Update ethtrustfund.sol

--- ethtrustfund.sol
+++ ethtrustfund.sol
@@ -2,76 +2,91 @@ pragma solidity 0.8.16;
 
 contract EthTrustFund {
 
-  address public owner;
-  
-  struct Beneficiary {
-    address payable walletAddress;
-    string name;
-    uint releaseTime;
-    uint amount;
-  }
+ // Setup Owner  
+    constructor() {
+        owner = msg.sender;
+    }
 
-  Beneficiary[] public beneficiaries; 
+    address public owner;
 
-  constructor() {
-    owner = msg.sender;
-  }
+ // Define Beneficiary
+    struct Beneficiary {
+        address payable walletAddress;
+        string name;
+        uint releaseTime;
+        uint amount;
+    }
 
-  function addBeneficiary(
-      address payable walletAddress,
-      string calldata name,
-      uint releaseTime
-  ) external {
-      require(msg.sender == owner, "You must be the owner to add a beneficiary.")
+    Beneficiary[] public beneficiaries; 
+
+ // Add Beneficiaries
+    function addBeneficiary(
+        address payable walletAddress,
+        string calldata name,
+        uint releaseTime
+    ) external {
+        require(msg.sender == owner, "You must be the owner to add a beneficiary.")
   
-      beneficiaries.push(
-        Beneficiary(walletAddress, name, releaseTime, 0)
-      );
-  }  
+        beneficiaries.push(
+            Beneficiary(walletAddress, name, releaseTime, 0)
+        );
+
+        emit BeneficiaryCreated(walletAddress, name);
+    }
+
+ // Deposit to Beneficiaries
+    function depositToBeneficiary(address walletAddress) external payable {
+        for(uint i = 0; i < beneficiaries.length; i++) {
+            if (beneficiaries[i].walletAddress == walletAddress)  {
+                beneficiaries[i].amount += msg.value;
+            }
+        }
+    }
   
-  function depositToBeneficiary(address walletAddress) external payable {
-      for(uint i = 0; i < beneficiaries.length; i++) {
-          if (beneficiaries[i].walletAddress == walletAddress)  {
-              beneficiaries[i].amount += msg.value;
-          }
-      }
-  }
-  
-  function contractBalance() external view returns (uint) {
-      return address(this).balance;
-  }
-  
-  function availableToWithdraw(address walletAddress) public view returns(bool) {
-      (uint i, bool ok) = _getIndex(walletAddress);
-      require(ok, "Beneficiary does not exist."0); 
+ // Get Contract Balance
+    function contractBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+ // Check If We Can Withdraw
+    function availableToWithdraw(address walletAddress) public view returns(bool) {
+        (uint i, bool ok) = _getIndex(walletAddress);
+        require(ok, "Beneficiary does not exist."); 
+
+        if (block.timestamp > beneficiaries[i].releaseTime) {
+            return true;
+        }
       
-      if (block.timestamp > beneficiaries[i].releaseTime) {
-          return true;
-      }
-      
-      return false;
-  }
-  
+        return false;
+    }
+
+ // Withdraw
+    function withdraw() external {
+        (uint i, bool ok) = _getIndex(msg.sender);
+        require(ok, "Beneficiary does not exist");
+        require(availableToWithdraw(msg.sender), "Not able to withdraw");
+    
+        Beneficiary storage b = beneficiaries[i];
+        uint amount = b.amount;
+        b.amount = 0;
+        b.walletAddress.transfer(amount);
+    }
+
+ // View Beneficiaries
   function _getIndex(address walletAddress) private view returns(uint, bool) {
-      for(uint i = 0; i < beneficiaries.length; i++) {
-          if (beneficiaries[i].walletAddress == walletAddress) {
-              return (i, true); 
-      }
+    for(uint i = 0; i < beneficiaries.length; i++) {
+        if (beneficiaries[i].walletAddress == walletAddress) {
+            return (i, true); 
+        }
       
       return (type(uint).max, false);
+    }
   }
 }
 
 
+// Extra
 
-/// Get Contract Balance
+// Events
 
-/// Check If We Can Withdraw
-
-/// Withdraw
-
-/// Extra
-
-/// Events
-
-/// Modifiers
+// Modifiers
